051f200ffe41c49ded19745c6213e45086c0f41f
Thomas Peters <tom@tjpeters.net>
2015-05-30 13:47:36 +1000

    Fix up 32 bit constants.

--- ShellHookLauncher32/ShellHookConstants.cs
+++ ShellHookLauncher32/ShellHookConstants.cs
@@ -2,7 +2,7 @@
 {
     internal static partial class ShellHookHelper
     {
-        public const string DllFileName = "ShellHook64.dll";
-        public const string PanelessNamedPipe = "PanelessHookId64-5b4f1ea2-c775-11e2-8888-47c85008ead5";
+        public const string DllFileName = "ShellHook32.dll";
+        public const string PanelessNamedPipe = "PanelessHookId32-5b4f1ea2-c775-11e2-8888-47c85008ead5";
     }
 }
